ea78454806905fb091fbf3283b9a0de310e940f0
dezorze <lfdprogram@gmail.com>
2025-10-24 20:24:15 -0300

    Habilidades posicionadas no tabuleiro

--- desafio-batalha-naval.c
+++ desafio-batalha-naval.c
@@ -1,37 +1,86 @@
-# include <stdio.h>
+#include <stdio.h>
 
-int main() {
-    int linhas = 10;
-    int cols = 10;
-    int tabela[10][10] = {}; //primeiro índice = linha.
-
-    //Terceiro Passo: inserido o navio 1 as colunas D, E e F, na linha 3. 
-    for(int i = 3; i <= 5; i++) {
-        tabela[2][i]=3;
+//Posicionando o prmeiro navio em D, linhas 4,5,6:
+void firstship(int table[10][10]) {
+    for(int i = 3; i < 6; i++) {
+        table[i][3] = 3;
     }
+}
 
-    //Quarto passo: inserindo o navio 2 as linhas 5, 6 e 7, na coluna H.
-    for(int i = 4; i <= 6; i++) {
-        tabela[i][7] = 4;
+//Posicionando o segundo navio H,I,J, linha 9:
+void secondship(int table[10][10]) {
+    for(int i = 7; i < 10; i++) {
+        table[8][i] = 3;
     }
+}
 
-    
-    //Primeiro Passo: criando as colunas:
-    printf("   ");
-    for(int i = 0; i < cols; i++) {
+// Função para criar as colunas (A, B, C...)
+void columns(int cols) {
+    printf("   "); // Espaço inicial para alinhar
+    for (int i = 0; i < cols; i++) {
         printf(" %c", 'A' + i);
     }
     printf("\n");
+}
 
-    //Segundo Passo: criando agora as linhas:
-    for(int j = 0; j < linhas; j++) {
-        printf("%2d ", 1 + j);
-        for(int i = 0; i < cols; i++) {
-            printf("%2d", tabela[j][i]);
+// Função para imprimir as linhas e a tabela
+void lines(int linhas, int cols, int table[10][10]) {
+    for (int j = 0; j < linhas; j++) {
+        printf("%2d ", 1 + j); // Número da linha
+        for (int i = 0; i < cols; i++) {
+            printf("%2d", table[j][i]);
         }
-
         printf("\n");
     }
+}
+
+//Habilidades
+void cone(int table[10][10]) {
+    
+}
+
+//Octaedro
+void octaedro(int table[10][10]) {
+    for(int i = 0; i < 3; i++){
+        table[1][i] = 3;
+        for(int j = 0; j < 3; j++) {
+            table[j][1] = 3;
+        }
+    }
+}
+
+//Cruz
+void cruz(int table[10][10]) {
+    for(int i = 5; i < 10; i++){
+        table[1][i] = 3;
+        for(int j = 0; j < 3; j++) {
+            table[j][7] = 3;
+        }
+    }
+}
+
+
+int main() {
+    int cols = 10;
+    int linhas = 10;
+    int table[10][10];
+    
+    //Inicializando a table para evitar lixo na memoria
+    for(int i = 0; i < linhas; i++) {
+        for(int j = 0; j < cols; j++) {
+            table[i][j] = 0;
+        }
+    }
+
+
+    // Exibe o tabuleiro
+    cone(table);
+    cruz(table);
+    octaedro(table);
+    firstship(table);
+    secondship(table);
+    columns(cols);
+    lines(linhas, cols, table);
 
     return 0;
 }
